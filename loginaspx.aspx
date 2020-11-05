dd1b63ec586b570541174f000e6837454ac89e29
Long-Tranf <65438882+Long-Tranf@users.noreply.github.com>
2020-11-05 08:00:17 +0700

    Add files via upload

---
+++ loginaspx.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="loginaspx.aspx.cs" Inherits="bookShopProject.loginaspx" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="login.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div class="abc">
+                <h1 class="wc">
+                    <img src="img/logo.jpg" /><br />
+                    WELCOME!
+                </h1>
+                <asp:Login ID="Login1" runat="server" OnAuthenticate="Login1_Authenticate"></asp:Login>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ loginaspx.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace bookShopProject
+{
+    public partial class loginaspx : System.Web.UI.Page
+    {
+        string stcn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\ASP\DO_AN_ASP\DO_AN_ASP\App_Data\Database1.mdf;Integrated Security=True";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+            string ten = this.Login1.UserName;
+            string matkhau = this.Login1.Password;
+            string sql = "select * from TK where TenDN='" + ten + "' and MatKhau = '" + matkhau + "'";
+            DataTable table = new DataTable();
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter(sql, stcn);
+                da.Fill(table);
+            }
+            catch (SqlException err)
+            {
+                Response.Write("<b>Error</b>" + err.Message + "<p/> ");
+            }
+            if (table.Rows.Count != 0)
+            {
+                Response.Cookies["TenDN"].Value = ten;
+                Server.Transfer("register.aspx");
+            }
+            else this.Login1.FailureText = "Tên Đăng Nhập hay Mật Khẩu không đúng! ";
+        }
+    }
+}
